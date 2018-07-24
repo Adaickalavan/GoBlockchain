c7af2b4b16893b2840805916b55b1b38f6d57a7a
Adaickalavan <adaickalavan@gmail.com>
2018-07-25 06:24:27 +0800

    Update

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"time"
 )
 
 // Block represents a single block in the blockchain
@@ -18,4 +21,41 @@ var Blockchain []Block
 
 func main() {
 	fmt.Println("hi")
+}
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+	t := time.Now()
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+	switch {
+	case oldBlock.Index+1 != newBlock.Index:
+		return false
+	case oldBlock.Hash != newBlock.PrevHash:
+		return false
+	case calculateHash(newBlock) != newBlock.Hash:
+		return false
+	}
+	return true
+}
+
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
 }
